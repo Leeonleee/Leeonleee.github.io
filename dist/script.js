cf9028d9f46f3ac320b4e474ff316ca431fbed13
Leon Lee <leonlee20031219@gmail.com>
2023-04-12 22:16:16 +1000

    trying to add persistent dark mode

--- dist/script.js
+++ dist/script.js
@@ -17,24 +17,25 @@
 function toggle_dark_mode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-    var url = new URL(window.location.href);
     if (element.classList.contains("dark-mode")) {
-      url.searchParams.set("darkModeEnabled", "true");
+      localStorage.setItem("darkModeEnabled", "true");
     } else {
-      url.searchParams.delete("darkModeEnabled");
+      localStorage.removeItem("darkModeEnabled");
     }
-    window.history.replaceState(null, "", url);
   }
+  
   
 
 
-document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
     var element = document.body;
-    var url = new URL(window.location.href);
-    if (url.searchParams.get("darkModeEnabled") === "true") {
-        element.classList.add("dark-mode");
-    } else {
-        element.classList.remove("dark-mode");
+    var dark_mode_query = localStorage.getItem("darkModeEnabled") === "true" ? "?darkModeEnabled=true" : "";
+    if (dark_mode_query !== "") {
+      element.classList.add("dark-mode");
     }
-});
+    document.querySelectorAll("nav a").forEach(function(link) {
+      link.href += dark_mode_query;
+    });
+  });
+  
   
