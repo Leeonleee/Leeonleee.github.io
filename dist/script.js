96735bce3c650d30109953f732d900ed503d10db
Leon Lee <leonlee20031219@gmail.com>
2023-04-12 21:59:16 +1000

    trying to make dark mode persistent

--- dist/script.js
+++ dist/script.js
@@ -3,14 +3,13 @@
 //     element.classList.toggle("dark-mode");
 //  }
 
-
 function toggle_dark_mode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
     if (element.classList.contains("dark-mode")) {
-        localStorage.setItem("darkModeEnabled", "true");
+      sessionStorage.setItem("darkModeEnabled", "true");
     } else {
-        localStorage.removeItem("darkModeEnabled");
+      sessionStorage.removeItem("darkModeEnabled");
     }
-}
+  }
   
