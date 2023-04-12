939474c2e37a53aeb560d0755b1ee355de63f960
Leon Lee <leonlee20031219@gmail.com>
2023-04-12 21:41:45 +1000

    added smooth scroll

--- dist/script.js
+++
@@ -1,8 +0,0 @@
-function smoothScroll(target) {
-    var element = document.querySelector(target);
-    window.scrollTo({
-      behavior: 'smooth',
-      left: 0,
-      top: element.offsetTop
-    });
-  }
