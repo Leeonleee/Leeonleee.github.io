e9666abb73a88b732b7ff8d5ddc4b0056ab3691f
Leon Lee <leonlee20031219@gmail.com>
2024-05-05 13:42:02 +1000

    Added Emulator Project

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function contact(event) {
     .catch(() => {
       loading.classList.remove("modal__overlay--visible");
       alert(
-        "The email service is temporarily unavailable. Please contact me directly on contact@leonlee.io"
+        "The email service is temporarily unavailable. Please contact me directly on contact@leonlee.io or on LinkedIn"
       );
     });
 }
